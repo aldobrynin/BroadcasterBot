e7e888d653307b841d062aba5c979d3910215258
Alexander Dobrynin <dobrynin.lpv@gmail.com>
2017-03-23 17:49:36 +0500

    init commit

---
+++ BroadcasterBot/Dialogs/RootDialog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace BroadcasterBot.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var activity = await result;
+
+            var length = (activity.Text ?? string.Empty).Length;
+
+            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
