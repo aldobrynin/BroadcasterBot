2d4d4bf573f07483ef83b5d200e7912b9120380c
Alexander Dobrynin <dobrynin.lpv@gmail.com>
2017-03-26 14:28:57 +0500

    disposing dbcontext correctly

--- BroadcasterBot/Data/UsersConversationsRepository.cs
+++ BroadcasterBot/Data/UsersConversationsRepository.cs
@@ -1,30 +1,49 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BroadcasterBot.Data
 {
-    public class UsersConversationsRepository : IUsersConversationsRepository
+    public class UsersConversationsRepository : IUsersConversationsRepository, IDisposable
     {
+        private readonly UsersConversationsContext _context;
+        private bool _disposed;
+
+        public UsersConversationsRepository()
+        {
+            _context = new UsersConversationsContext();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
         public Task<IEnumerable<SavedConversationDto>> GetAllUsers()
         {
-            using (var db = new UsersConversationsContext())
-            {
-                return Task.FromResult(db.UsersConversations.AsEnumerable());
-            }
+            return Task.FromResult(_context.UsersConversations.AsEnumerable());
         }
 
         public async Task AddUser(SavedConversationDto conversation)
         {
-            using (var db = new UsersConversationsContext())
+            _context.UsersConversations.AddOrUpdate(conversation);
+            await _context.SaveChangesAsync();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed == false)
             {
-                if (db.UsersConversations.Any(x => x.UserId == conversation.UserId) == false)
+                if (disposing)
                 {
-                    db.UsersConversations.Add(conversation);
-                    await db.SaveChangesAsync();
+                    _context.Dispose();
                 }
             }
+            _disposed = true;
         }
     }
 
